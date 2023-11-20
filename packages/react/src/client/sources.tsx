df3cd6d2d0648e474d2dc15fea5210a29e55d6d5
Wade Fletcher <wbfletch@gmail.com>
2023-11-20 15:47:38 -0800

    fix: tailwind

--- packages/react/src/client/sources.tsx
+++ packages/react/src/client/sources.tsx
@@ -130,7 +130,7 @@ function SourceCardList({ query }: { query: string }) {
   const sources = useSources();
 
   return (
-    <div className="flex-1 border rounded-lg divide-y overflow-y-auto">
+    <div className="tb-flex-1 tb-border tb-rounded-lg tb-divide-y tb-overflow-y-auto">
       {sources
         .filter((source) =>
           source.name.toLowerCase().includes(query.toLowerCase()),
@@ -152,26 +152,29 @@ function ListSources() {
   const [query, setQuery] = useState('');
 
   return (
-    <Card className="shadow-xl h-[32rem] w-[24rem] flex flex-col">
+    <Card className="tb-shadow-xl tb-h-[32rem] tb-w-[24rem] tb-flex tb-flex-col">
       <CardHeader>
         <CardTitle>Connect Data Sources</CardTitle>
         <CardDescription>
           Connect your data sources to get started.
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 flex flex-col overflow-auto pt-1">
+      <CardContent className="tb-flex-1 tb-flex tb-flex-col tb-overflow-auto tb-pt-1">
         <Input
-          className="mb-4"
+          className="tb-mb-4"
           placeholder="Search..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
         <SourceCardList query={query} />
       </CardContent>
-      <CardFooter className="flex-col gap-3 items-start">
+      <CardFooter className="tb-flex-col tb-gap-3 tb-items-start">
         <p className="text-sm">
           Powered by{' '}
-          <a href="//wadefletcher.com" className="text-primary font-semibold">
+          <a
+            href="//wadefletcher.com"
+            className="tb-text-primary tb-font-semibold"
+          >
             Tractorbeam
           </a>
         </p>
@@ -186,22 +189,22 @@ function SourceOAuth2Start() {
   if (!source) return null;
 
   return (
-    <Card className="shadow-xl h-[32rem] w-[24rem] flex flex-col">
+    <Card className="tb-shadow-xl tb-h-[32rem] tb-w-[24rem] tb-flex tb-flex-col">
       <CardHeader>
         <CardTitle>Connect to {source.name}</CardTitle>
         <CardDescription>
           You'll be sent to {source.name} to securely log in to your account.
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 flex flex-col gap-6">
-        <div className="flex-1 flex items-center justify-center h-full bg-secondary rounded-lg border shadow-inner">
-          <div className="w-24 p-4 rounded-lg border bg-white z-10 shadow-sm">
+      <CardContent className="tb-flex-1 tb-flex tb-flex-col tb-gap-6">
+        <div className="tb-flex-1 tb-flex tb-items-center tb-justify-center tb-h-full tb-bg-secondary tb-rounded-lg tb-border tb-shadow-inner">
+          <div className="tb-w-24 tb-p-4 tb-rounded-lg tb-border tb-bg-white tb-z-10 tb-shadow-sm">
             <img src={source.iconSrc} alt={`${source.name} Icon`} />
           </div>
 
-          <div className="h-[2px] bg-ring w-12"></div>
+          <div className="tb-h-[2px] tb-bg-ring w-12"></div>
 
-          <div className="w-24 p-4 rounded-lg bg-white z-10 border shadow-sm">
+          <div className="tb-w-24 tb-p-4 tb-rounded-lg tb-bg-white tb-z-10 tb-border tb-shadow-sm">
             <img
               src={'https://logo.clearbit.com/github.com'}
               alt="GitHub Icon"
@@ -210,13 +213,16 @@ function SourceOAuth2Start() {
         </div>
         <a href={source.connectLink} className={cn(buttonVariants(), 'w-full')}>
           Continue to {source.name}
-          <ExternalLink className="w-4 h-4 mb-0.5 ml-1" />
+          <ExternalLink className="tb-w-4 tb-h-4 tb-mb-0.5 tb-ml-1" />
         </a>
       </CardContent>
-      <CardFooter className="flex-col gap-3 items-start">
+      <CardFooter className="tb-flex-col tb-gap-3 tb-items-start">
         <p className="text-sm ">
           Powered by{' '}
-          <a href="//wadefletcher.com" className="text-primary font-semibold">
+          <a
+            href="//wadefletcher.com"
+            className="tb-text-primary tb-font-semibold"
+          >
             Tractorbeam
           </a>
         </p>
@@ -231,26 +237,29 @@ function SourceOauth2Complete() {
   if (!source) return null;
 
   return (
-    <Card className="shadow-xl h-[32rem] w-[24rem] flex flex-col">
+    <Card className="tb-shadow-xl tb-h-[32rem] tb-w-[24rem] tb-flex tb-flex-col">
       <CardHeader>
         <CardTitle>Connection Successful</CardTitle>
         <CardDescription>
           Your {source.name} account has been connected!
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 flex-col flex gap-3">
-        <div className="flex-1 flex justify-center items-center">
-          <div className="rounded-full p-6 bg-primary/10">
-            <CheckCircle className="w-8 h-8 text-primary" />
+      <CardContent className="tb-flex-1 tb-flex-col tb-flex tb-gap-3">
+        <div className="tb-flex-1 tb-flex tb-justify-center tb-items-center">
+          <div className="tb-rounded-full tb-p-6 tb-bg-primary/10">
+            <CheckCircle className="tb-w-8 tb-h-8 tb-text-primary" />
           </div>
         </div>
         <Button variant="secondary">Add Another Data Source</Button>
         <Button>Done</Button>
       </CardContent>
-      <CardFooter className="flex-col gap-3 items-stretch">
-        <p className="text-sm ">
+      <CardFooter className="tb-flex-col tb-gap-3 tb-items-stretch">
+        <p className="tb-text-sm ">
           Powered by{' '}
-          <a href="//wadefletcher.com" className="text-primary font-semibold">
+          <a
+            href="//wadefletcher.com"
+            className="tb-text-primary tb-font-semibold"
+          >
             Tractorbeam
           </a>
         </p>
@@ -261,7 +270,7 @@ function SourceOauth2Complete() {
 
 export function Sources() {
   return (
-    <div className="m-10 w-full flex gap-10 antialiased">
+    <div className="tb-m-10 tb-w-full tb-flex tb-gap-10 tb-antialiased">
       <ListSources />
       <SourceOAuth2Start />
       <SourceOauth2Complete />
